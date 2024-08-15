bdc068d4a4954377b5cbb57591820870b2de5aeb
sam0404 <35382713+sam0404@users.noreply.github.com>
2024-08-15 15:12:53 +0300

    init

---
+++ index.e28ca.js
@@ -0,0 +1,25 @@
+System.register(["./application.91295.js"], function (_export, _context) {
+  "use strict";
+
+  var Application, application;
+
+  function topLevelImport(url) {
+    return System["import"](url);
+  }
+
+  return {
+    setters: [function (_application91295Js) {
+      Application = _application91295Js.Application;
+    }],
+    execute: function () {
+      application = new Application();
+      topLevelImport('cc').then(function (engine) {
+        return application.init(engine);
+      }).then(function () {
+        return application.start();
+      })["catch"](function (err) {
+        console.error(err);
+      });
+    }
+  };
+});
