9609e6f20af02a3b19146be96296b7eee5f978d6
sam0404 <35382713+sam0404@users.noreply.github.com>
2024-08-15 17:57:27 +0300

    fix settings

--- cocos-js/wait-for-ammo-instantiation.js
+++ cocos-js/wait-for-ammo-instantiation.js
@@ -1,1 +1,2 @@
 System.register(["./instantiated-4fa52c58.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.h_)}],execute:function(){}}}));
+//# sourceMappingURL=wait-for-ammo-instantiation.js.map
